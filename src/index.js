2244af14ac99caa08f72a5f96204b042365832e3
Guilherme Coelho Garnizet <83613881+guicoelhoo@users.noreply.github.com>
2026-04-01 00:23:27 -0300

    Correção Sonar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const calculadora = require('./src/calculadora');
 
-console.log(calculadora.soma(3, 3));
+console.log(calculadora.soma(2, 3));
 console.log(calculadora.multiplicacao(2, 3));
 console.log(calculadora.subtracao(2, 3));
